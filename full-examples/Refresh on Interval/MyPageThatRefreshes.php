abc3bf1e5de7799dcfbc38050547c0a76494297d
eneerge <eneerge@eneerge.net>
2018-12-07 08:02:23 -0600

    Create MyPageThatRefreshes.php

---
+++ full-examples/Refresh on Interval/MyPageThatRefreshes.php
@@ -0,0 +1,22 @@
+<!-- HTML OF PAGE -->
+
+<!-- Now setup Unorthodox AJAX -->
+<script type="text/javascript" src="UnorthodoxAjax.js"></script>
+<script type="text/javascript">
+// Implement my response or other functions
+function asyncProcessResponse(obj) {
+  alert("Hey guys, the data was just received and it is available in obj");
+  // Do some more stuff with the response obj
+}
+
+// Now lets wireup the refreshing interval
+window.addEventListener(
+  "load"
+  ,function() {
+    asyncRefresh("checkForUpdates.php",10000); // poll every 10 seconds
+  }
+  ,false
+);
+
+// Now a script tag will be removed and recreated every 10 seconds that polls checkForUpdates.php
+</script>
